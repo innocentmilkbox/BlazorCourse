c3b4b61fdd84a21c464b0e538daf95197ad29e3a
QuangHuyZ <patrotothenus@gmail.com>
2021-07-26 14:51:57 +0700

    initial

---
+++ BlazorApp/BlazorApp_Server/Models/Amenitie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp_Server.Models
+{
+    public class Amenitie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descriptions { get; set; }
+    }
+}
